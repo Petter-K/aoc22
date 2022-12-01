dfe26581d754166498b8e3f289085ceff04a7385
= <=>
2022-12-01 22:40:30 +0100

    fix

--- app/Task1.hs
+++ app/Task1.hs
@@ -1,20 +1,20 @@
 module Task1 (solution1, solution2) where
 
-import Data.List (sort)
+import Data.List (sortOn)
 
 solution1 :: IO Int
 solution1 = maximum <$> input
 
 solution2 :: IO Int
-solution2 = sum . take 3 . reverse . sort <$> input
+solution2 = sum . take 3 . sortOn negate <$> input
 
 input :: IO [Int]
 input = do
   file <- readFile "resources/input1.txt"
-  return $ map (sum . map read) (splitOnSpace $ lines file)
+  return $ map (sum . map read) (splitInput $ lines file)
 
-splitOnSpace :: [String] -> [[String]]
-splitOnSpace f = go f [] []
+splitInput :: [String] -> [[String]]
+splitInput f = go f [] []
   where go :: [String] -> [String] -> [[String]] -> [[String]]
         go [] l a = l : a
         go (x:xs) l a = if x == "" then go xs [] (l:a) else go xs (x:l) a
