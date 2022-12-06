75d8851341782e1daeccbb653cda8336f8425fc7
= <=>
2022-12-06 19:26:13 +0100

    task 5 finally

--- app/Task5.hs
+++ app/Task5.hs
@@ -1,31 +1,45 @@
 module Task5 (sol1, sol2) where
 
-import Data.List (transpose)
+import Data.Char (isLetter, isDigit)
+import Data.List (transpose, foldl')
+import Data.List.Split (splitWhen)
+
 import qualified Data.Map as M
 
-sol1 = Nothing
-sol2 = Nothing
+type Command = [Int]
+type Crates = M.Map Int String
 
 
-parseCrates s = 1
-  where f :: String -> M.Map Int String -> M.Map Int String
-        f s m = m 
+sol1 :: IO String
+sol1 = do
+  crates <- initialCrates
+  cmds <- commands
+  let res = foldl' handleCommandSingle crates cmds
+  return (map (head . snd) (M.assocs res))
 
-input :: IO ([String], [String])
-input = do 
-  file <- lines <$> readFile "resources/input5.txt"
-  let crates = takeWhile (/= "") file
-  let cmds = drop (length crates + 2) file -- Dropping 1 2 3 row and an empty line. 
-  return (crates, cmds)
+sol2 :: IO String
+sol2 = do
+  crates <- initialCrates
+  cmds <- commands
+  let res = foldl' handleCommandMulti crates cmds
+  return (map (head . snd) (M.assocs res))
 
+handleCommandSingle :: Crates -> Command -> Crates
+handleCommandSingle m (0:_) = m
+handleCommandSingle m [c,f,t] = let (x:xs) = m M.! f
+                          in handleCommandSingle (M.adjust (x :) t (M.insert f xs m)) [c-1, f, t]
+
+handleCommandMulti :: Crates -> Command -> Crates
+handleCommandMulti m (0:_) = m
+handleCommandMulti m [c,f,t] = let l = m M.! f
+                               in M.adjust (take c l ++) t (M.insert f (drop c l) m)
+
+
+commands :: IO [Command]
+commands = map (map read . filter (not . null) . splitWhen (not . isDigit)) . drop 10 . lines <$> file
+
+initialCrates :: IO Crates
+initialCrates = M.fromList . zip [1..] . filter (not . null) . map (filter isLetter) . transpose . takeWhile (/= "") . lines <$> file
+
+file :: IO String
 file = readFile "resources/input5.txt"
-
-
-test = do
-  (h,_) <- input
-  return h
-
--- transpose . take 9 . lines <$> file
--- group :: Int -> [String] -> [[String]]
-group _ [] = []
-group n l = take n l : group n (drop n l)
